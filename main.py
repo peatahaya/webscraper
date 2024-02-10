6f587a6a344471ba7f4c2ed9563cba3abaa2001e
peatahaya <pantherbielicki2@vp.pl>
2024-02-10 12:29:09 +0100

    Adding course_price and new print function

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ with open('home.html', 'r') as html_file:
     #
     # for course in courses_html_tags:
     #     print(course.text)
+    courses = soup.find_all('div', class_='card')
+    for course in courses:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        # print(course_name)
+        # print(course_price)
+        print(f'{course_name} costs {course_price}')
+
 
