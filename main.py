1d2c7738ea348f2d62861ed779a58f376c1947fd
peatahaya <pantherbielicki2@vp.pl>
2024-02-10 12:18:37 +0100

    updating main.py

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from bs4 import BeautifulSoup
 with open('home.html', 'r') as html_file:
     content = html_file.read()
 
-    print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
+    # courses_html_tags = soup.find_all('h5')
+    # print(courses_html_tags)
+    #
+    # for course in courses_html_tags:
+    #     print(course.text)
+
